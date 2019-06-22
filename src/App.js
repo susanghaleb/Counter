ec7def8c823dd14b3ef1113a8c693ece0de198b8
susan <susanaghaleb@gmail.com>
2019-06-22 16:38:27 -0300

    counter increment & decrement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Counter extends Component{
+  state={
+    count:0
+  }
+
+  handleIncrement=()=>{
+    this.setState({
+      count:this.state.count +1
+    })
+  }
+
+  handleDecrement=()=>{
+    this.setState({
+      count:this.state.count -1
+    })
+  }
+  render(){
+    return(
+      <div>
+        <div>count: {this.state.count}</div>
+         <button onClick={this.handleIncrement}>Incrementar</button>
+         <button onClick={this.handleDecrement}>Decrementar</button>
+      </div>
+    )
+  }
 }
 
+class App extends Component{
+  render(){
+    return(
+      <Counter></Counter>
+    )
+  }
+}
 export default App;
